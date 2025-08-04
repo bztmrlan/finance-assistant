c53468b7d780cede71a24f921e8e5c4b4c5d8cb6
bztmrlan <temirlan.bazarzhan@gmail.com>
2025-08-05 04:27:38 +0500

    chore: scaffold Spring Boot project structure

---
+++ finance-assistant-backend/src/main/java/com/github/bztmrlan/financeassistant/FinanceAssistantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.bztmrlan.financeassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAssistantBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceAssistantBackendApplication.class, args);
+	}
+
+}
---
+++ finance-assistant-backend/src/test/java/com/github/bztmrlan/financeassistant/FinanceAssistantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bztmrlan.financeassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceAssistantBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
